344385f2a81a297bf9492291a6a98274c0772937
Hitesh Rathi <97667730+HiteshRathi02@users.noreply.github.com>
2025-06-06 16:21:32 +0000

    updated id

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,29 @@
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { AuthProvider } from "./AuthContext";
-import Auth from "./Auth";
+import React, { useState, useContext, createContext } from "react";
 import './../styles/App.css';
 
+const AuthContext = createContext();
+
 const App = () => {
+  const [toggle, setToggle] = useState(false);
   return (
-    <div>
-    <AuthProvider>
-      <Auth />
-    </AuthProvider>
-    </div>
+    <AuthContext.Provider value={{toggle, setToggle}}>
+        <AuthComponent />
+    </AuthContext.Provider>
   )
 }
 
 export default App
+
+
+function AuthComponent(){
+  const {toggle, setToggle} = useContext(AuthContext);
+  return (
+    <div>
+      <h1>Click on the checkbox to get  authenticated</h1>
+      <p className="authText">{toggle ? "You are now authenticated, you can proceed": "you are not authenticated"}</p>
+      
+      <input type="checkbox" onClick={()=>{setToggle(!toggle)}}/>I am not a robot
+    </div>
+  )
+}
